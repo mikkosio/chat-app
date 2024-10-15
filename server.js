6ae6be451c9f85db36ad7f37c9a30f9268393c76
Mikko Sio <msio1@my.bcit.ca>
2024-10-15 01:34:08 -0700

    connected to socket

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const server = require("http").createServer();
+const express = require("express")
+const app = express()
+const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-
 const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
 
 io.on("connection", client => {
     client.on("event", data => {
