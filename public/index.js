579e7aa427c58f646a13b70f675f5abf84a81ddb
Mikko Sio <msio1@my.bcit.ca>
2024-10-21 17:47:17 -0700

    implemented usernames

--- public/index.js
+++ public/index.js
@@ -1,8 +1,72 @@
-const socket = io();
+let socket;
 
-const button = document.getElementById("send-btn");
+const usernameContainer = document.getElementById("username-container")
+const usernameInput = document.getElementById("username");
+const joinBtn = document.getElementById("join-btn")
+
+const chat = document.getElementById("chat");
+const sendBtn = document.getElementById("send-btn");
 const inputBox = document.getElementById("input");
-const chatBox = document.getElementById("chat-box")
+const chatBox = document.getElementById("chat-box");
+
+function joinChat() {
+    const username = usernameInput.value.trim();
+
+    if (username) {
+        socket = io();
+    
+        usernameContainer.style.display = "none";
+        chat.style.display = "flex";
+    
+        socket.emit("join", username)
+
+        // handle message events
+        socket.on("message", (client, msg) => {
+            const date = new Date()
+
+            const day = String(date.getDate()).padStart(2, "0");
+            const month = String(date.getMonth() + 1).padStart(2, "0");
+            const year = date.getFullYear();
+            let hours = date.getHours();
+            const minutes = String(date.getMinutes()).padStart(2, "0");
+            const period = hours >= 12 ? "PM" : "AM";
+            hours = hours % 12 || 12;
+
+            const timestamp = `${day}/${month}/${year} ${hours}:${minutes} ${period}`;
+
+            const messageItem = document.createElement("li");
+            messageItem.innerHTML = `
+                <div class="message">
+                    <span class="username">${client}</span>
+                    <span class="message-timestamp">${timestamp}</span>
+
+                    <div>${msg}</div>
+                </div>
+            `
+
+            chatBox.prepend(messageItem);
+        })
+
+        // handle system events
+        socket.on("system", msg => {
+            const messageItem = document.createElement("li");
+            messageItem.innerHTML = `
+                <div class="message">
+                    <strong>${msg}</strong>
+                </div>
+            `
+
+            chatBox.prepend(messageItem);
+        })
+    }
+}
+
+joinBtn.addEventListener("click", joinChat)
+usernameInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        joinChat();
+    }
+})
 
 function sendMessage() {
     const message = inputBox.value;
@@ -12,35 +76,9 @@ function sendMessage() {
         inputBox.value = "";
     }
 }
-button.addEventListener("click", sendMessage)
+sendBtn.addEventListener("click", sendMessage)
 inputBox.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
         sendMessage();
     }
-})
-
-socket.on("message", (client, msg) => {
-    const date = new Date()
-
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const year = date.getFullYear();
-    let hours = date.getHours();
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const period = hours >= 12 ? "PM" : "AM";
-    hours = hours % 12 || 12;
-
-    const timestamp = `${day}/${month}/${year} ${hours}:${minutes} ${period}`;
-
-    const messageItem = document.createElement("li");
-    messageItem.innerHTML = `
-        <div class="message">
-            <span class="username">${client}</span>
-            <span class="message-timestamp">${timestamp}</span>
-
-            <div>${msg}</div>
-        </div>
-    `
-
-    chatBox.prepend(messageItem);
 })
--- server.js
+++ server.js
@@ -4,16 +4,33 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const port = process.env.PORT || 3000;
 
+const users = {};
+
 app.use(express.static("public"));
 
 io.on("connection", client => {
+    client.on("join", username => {
+        console.log(`${username} has joined.`);
+
+        users[client.id] = username;
+
+        io.emit("system", `${username} has joined the chat`)
+    })
+
     client.on("message", msg => {
         console.log(`${client.id} says ${msg}`);
-        io.emit("message", client.id, msg)
+
+        const username = users[client.id] || "Unknown User"
+
+        io.emit("message", username, msg)
     })
     
     client.on("disconnect", () => {
-        console.log(`${client.id} has left.`);
+        const username = users[client.id] || "Unknown User"
+
+        console.log(`${username} has left.`);
+
+        io.emit("system", `${username} has left the chat`)
     })
 })
 
